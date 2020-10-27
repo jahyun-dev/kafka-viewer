16d95ddb7014564cc516a4b370fc2c1294a97eeb
jahyun.dev <jahyun.dev@gmail.com>
2020-10-27 21:58:36 +0900

    Fix ui

--- src/cljs/kafka_viewer/topics/view.cljs
+++ src/cljs/kafka_viewer/topics/view.cljs
@@ -6,7 +6,7 @@
   []
   (let [{:keys [keyword system?] :as topic-filter} @(rf/subscribe [:topic-filter])]
     [:div {:class "flex justify-between"}
-     [:div {:class "mt-1"}
+     [:div {:class "mt-1 relative"}
       [:div {:class "absolute inset-y-0 left-0 pl-3 flex items-center"}
        [:svg {:class "mr-3 h-4 w-4 text-gray-400" :viewBox "0 0 20 20" :fill "currentColor"}
         [:path {:fill-rule "evenodd" :d "M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" :clip-rule "evenodd"}]]]
